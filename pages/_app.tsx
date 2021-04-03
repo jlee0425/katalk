9d685c574139591b2f9eb1486f0f9deeef846630
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-03 21:54:21 +0900

    added theme & ssr styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@lib/firebase';
+import { UserContext } from '@lib/context';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		padding: 0;
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+			Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	* {
+		box-sizing: border-box;
+	}
+`;
+
+const theme = {
+	colors: {
+		kakaoBrown: '#3A1D1D',
+		kakaoYellow: '#F7E600',
+	},
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default MyApp;
