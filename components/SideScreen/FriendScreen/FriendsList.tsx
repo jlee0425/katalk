870a06553263ea4c6f4abb8d595026acfd53c5e2
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-10 21:15:24 +0900

    refactor

--- components/SideScreen/FriendScreen/FriendsList.tsx
+++ components/SideScreen/FriendScreen/FriendsList.tsx
@@ -1,39 +1,11 @@
-import { UserContext, UserProps } from '@lib/context';
-import { auth, firestore, getUserWithID } from '@lib/firebase';
-import { useUserData } from '@lib/hooks';
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import React, { useContext, useEffect, useState } from 'react';
+import { UserProps } from '@lib/context';
+import { useFriendList } from '@lib/hooks';
+import React from 'react';
 import styled from 'styled-components';
-import {
-	useCollection,
-	useDocumentDataOnce,
-} from 'react-firebase-hooks/firestore';
 import { UserCard } from './UserCard';
-import firebase from 'firebase/app';
 
 export const FriendsList = (props: any) => {
-	const [friendsSnapshot, loading] = useCollection(
-		firestore
-			.collection('users')
-			.doc(auth.currentUser!.uid)
-			.collection('friends'),
-	);
-	const [friends, setFriends] = useState<firebase.firestore.DocumentData>([]);
-
-	useEffect(() => {
-		const fetchFriendList = async () => {
-			const friendsIDs = friendsSnapshot!.docs.map((doc) => doc.id);
-
-			const users = await Promise.all(
-				friendsIDs.map((id) => getUserWithID(id)),
-			);
-			setFriends(users);
-		};
-
-		if (friendsSnapshot && !loading) {
-			fetchFriendList();
-		}
-	}, [friendsSnapshot, loading]);
+	const friends = useFriendList();
 
 	return (
 		<Container>
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,8 +1,9 @@
 import { UserProps } from './context';
 import { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, firestore, userConverter } from './firebase';
+import { auth, firestore, getUserWithID, userConverter } from './firebase';
 import firebase from 'firebase/app';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 export const useUserData = () => {
 	const [user, loading] = useAuthState(auth);
@@ -31,4 +32,31 @@ const addUserToDB = async (user: firebase.User): Promise<void> => {
 		.collection('users')
 		.doc(user.uid)
 		.set(userConverter(user), { merge: true });
+};
+
+export const useFriendList = () => {
+	const [friendsSnapshot, loading] = useCollection(
+		firestore
+			.collection('users')
+			.doc(auth.currentUser!.uid)
+			.collection('friends'),
+	);
+	const [friends, setFriends] = useState<firebase.firestore.DocumentData>([]);
+
+	useEffect(() => {
+		const fetchFriendList = async () => {
+			const friendsIDs = friendsSnapshot!.docs.map((doc) => doc.id);
+
+			const users = await Promise.all(
+				friendsIDs.map((id) => getUserWithID(id)),
+			);
+			setFriends(users);
+		};
+
+		if (friendsSnapshot && !loading) {
+			fetchFriendList();
+		}
+	}, [friendsSnapshot, loading]);
+
+	return friends;
 };
