60e5f6a504363a7878cc2ab34a141b1230e2e684
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-11 22:21:27 +0900

    init

---
+++ components/SideScreen/ChatScreen/UserCheckBox.tsx
@@ -0,0 +1,47 @@
+import { UserProps } from '@lib/context';
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+
+export const UserCheckBox = ({ username, photoURL }: Partial<UserProps>) => {
+	return (
+		<Container>
+			{photoURL ? (
+				<Avatar src={photoURL} />
+			) : (
+				<Avatar>{username![0].toUpperCase()}</Avatar>
+			)}
+			<UserDetails>
+				<h4>{username}</h4>
+			</UserDetails>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: row;
+	border-bottom: 1px solid #e6e3e3;
+	padding: 1rem 1rem;
+
+	&:hover {
+		background: #eeeeee;
+	}
+`;
+
+const UserDetails = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding-left: 10px;
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	h4 {
+		font-weight: 500;
+	}
+	p {
+		font-size: 12px;
+	}
+`;
