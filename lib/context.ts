6d72d198435663c6ef914072979121a1fcfccaaa
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-16 21:11:41 +0900

    added setState fn & changed type of 'lastSeen'

--- lib/context.ts
+++ lib/context.ts
@@ -1,20 +1,20 @@
-import FriendScreen from '@components/SideScreen/FriendScreen';
-import { serverTimestamp } from './firebase';
-import { createContext, Dispatch, ReactElement, SetStateAction } from 'react';
 import firebase from 'firebase/app';
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { serverTimestamp } from './firebase';
+import { format } from 'timeago.js';
 
 export interface UserProps {
 	username: string;
 	email: string;
 	photoURL: string;
-	lastSeen: firebase.firestore.FieldValue;
+	lastSeen: string;
 }
 
 export const defaultUser: UserProps = {
 	username: 'username',
 	email: 'email@email.com',
 	photoURL: '',
-	lastSeen: serverTimestamp(),
+	lastSeen: format(serverTimestamp().toString()),
 };
 
 export const UserContext = createContext<UserProps>(defaultUser);
@@ -28,12 +28,14 @@ export interface ScreenProps {
 	screen: SCREENS;
 	selectedElement: UserProps | null;
 	setScreen: Dispatch<SetStateAction<SCREENS>>;
+	setSelectedElement: Dispatch<SetStateAction<UserProps | null>>;
 }
 
 export const ScreenContext = createContext<ScreenProps>({
 	screen: SCREENS.FriendScreen,
 	selectedElement: null,
 	setScreen: () => SCREENS,
+	setSelectedElement: () => {},
 });
 
 export interface SelectFriendsProps {
