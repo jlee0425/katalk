b175a370904655b525e445ffbd3bb69f15662415
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-16 21:09:52 +0900

    change screenContext w/r/t btn click

--- components/Sidebar/TopBtns.tsx
+++ components/Sidebar/TopBtns.tsx
@@ -1,21 +1,35 @@
-import React, { useContext } from 'react';
+import React, { MouseEventHandler, useContext } from 'react';
 import styled from 'styled-components';
 import PersonIcon from '@material-ui/icons/Person';
 import ChatButbbleIcon from '@material-ui/icons/ChatBubble';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import IconButton from '@material-ui/core/IconButton';
-import { ScreenContext, SCREENS } from '@lib/context';
+import { ScreenContext, SCREENS, SelectFriendsContext } from '@lib/context';
 
-interface Props {}
+enum BUTTONS {
+	FriendsBtn,
+	ChatBtn,
+}
 
-export const TopBtns = (props: Props) => {
-	const { setScreen } = useContext(ScreenContext);
+export const TopBtns = () => {
+	const { setScreen, setSelectedElement } = useContext(ScreenContext);
+
+	// todo: type for 'e'
+	const handleClick = (e: any) => {
+		setSelectedElement(null);
+		setScreen(
+			e.currentTarget.name == BUTTONS.ChatBtn
+				? SCREENS.ChatScreen
+				: SCREENS.FriendScreen,
+		);
+	};
+
 	return (
 		<Container>
-			<IconButton onClick={() => setScreen(SCREENS.FriendScreen)}>
+			<IconButton onClick={handleClick} name={BUTTONS.FriendsBtn.toString()}>
 				<PersonIcon />
 			</IconButton>
-			<IconButton onClick={() => setScreen(SCREENS.ChatScreen)}>
+			<IconButton onClick={handleClick} name={BUTTONS.ChatBtn.toString()}>
 				<ChatButbbleIcon />
 			</IconButton>
 			{/* <IconButton>
