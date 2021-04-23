77235e76787c89e0c50c8d914908663557d0c514
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-23 17:13:59 +0900

    chatcard implementation

--- components/SideScreen/ChatScreen/ChatCard.tsx
+++ components/SideScreen/ChatScreen/ChatCard.tsx
@@ -1,10 +1,37 @@
-import { ChatProps } from '@lib/context';
-import React from 'react';
+import { CardContainer, CardDetail } from '@components/styledComponents';
+import { UserAvatar } from '@components/UserAvatar';
+import { ChatProps, UserContext } from '@lib/context';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import React, { useContext } from 'react';
 
 interface Props {
 	chat: ChatProps;
 }
 
 export const ChatCard = ({ chat }: Props) => {
-	return <div></div>;
+	const { lastActive, messages } = chat;
+	const userInfo = useContext(UserContext);
+	const chattees = chat.chattees.filter((c) => c.email != userInfo.email);
+	return (
+		<CardContainer>
+			<AvatarGroup max={2}>
+				{chattees.map((chattee) => (
+					<UserAvatar user={chattee} key={chattee.email} />
+				))}
+			</AvatarGroup>
+			<CardDetail>
+				<h4>
+					{chattees.map((chattee, i) => (
+						<span key={i}>
+							{i < chattees.length - 1
+								? chattee.username + ', '
+								: chattee.username + ' ' + chattees.length}
+						</span>
+					))}
+				</h4>
+				<p>{messages[messages.length - 1]?.message}</p>
+			</CardDetail>
+			<p>{lastActive.toString()}</p>
+		</CardContainer>
+	);
 };
