d181943a5fb11e01a75673a94ee662a9fb3e26db
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-28 23:58:00 +0900

    init

--- components/MainScreen/ChatRoom/ChatBubble.tsx
+++ components/MainScreen/ChatRoom/ChatBubble.tsx
@@ -1,7 +1,18 @@
+import { MessageProps, UserProps } from '@lib/context';
 import React from 'react';
+import styled from 'styled-components';
 
-interface Props {}
+interface Props {
+	user: UserProps;
+	message: MessageProps;
+}
 
-export const ChatBubble = (props: Props) => {
-	return <div></div>;
+export const ChatBubble = ({ user, message }: Props) => {
+	return (
+		<Container>
+			<p>{message} </p>
+		</Container>
+	);
 };
+
+const Container = styled.div``;
