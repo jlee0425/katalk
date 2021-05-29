29be72da210e8111734e2a7d8c0bccd97873ad1d
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-29 21:24:17 +0900

    ssr

--- components/MainScreen/ChatRoom/index.tsx
+++ components/MainScreen/ChatRoom/index.tsx
@@ -1,10 +1,31 @@
 import { ChatProps, ScreenContext, UserContext } from '@lib/context';
+import { firestore } from '@lib/firebase';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { Header } from './Header';
 import { InputField } from './InputField';
 import { Main } from './Main';
 
+export const getServerSideProps: GetServerSideProps = async (
+	context: GetServerSidePropsContext,
+) => {
+	const ref = firestore.collection('chats').doc(context.query.id);
+
+	const msgRes = await ref.collection('messages').orderBy('sent', 'asc').get();
+
+	const messages = msgRes.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+	const chatRes = await ref.get();
+	const chattees = chatRes.data()?.chattees;
+
+	return {
+		props: {
+			messages: JSON.stringify(messages),
+			chattees: chattees,
+		},
+	};
+};
 const index = () => {
 	const { selectedElement } = useContext(ScreenContext);
 	const { chattees, messages } = selectedElement as ChatProps;
